6181aa7844f5adcca77078c93e633779bfded414
Eric Gee <egee@wrightbev,com>
2025-06-20 11:40:21 -0400

    fixed password functions

--- script/document.js
+++ script/document.js
@@ -30,13 +30,78 @@ function move() {
 
 // Overview Password Function
 
-    function overviewlogin() {
-      const correctPassword = "mySecret123";
-      const input = document.getElementById("overview-password").value;
+function overviewlogin(event) {
+  event.preventDefault();
+  const correctPassword = "4430971";
+  const input = document.getElementById("overview-password").value;
 
-      if (input === correctPassword) {
-        window.location.href = "https://example.com";
-      } else {
-        alert("Incorrect password. Please try again.");
-      }
-    }
+  if (input === correctPassword) {
+    window.location.href = "4430971-overview.html";
+  } else {
+  }
+}
+
+// Universe Password Function
+
+function universelogin(event) {
+  event.preventDefault();
+  const correctPassword = "3790572";
+  const input = document.getElementById("universe-password").value;
+
+  if (input === correctPassword) {
+    window.location.href = "3790572-universe.html";
+  } else {
+  }
+}
+
+// Factions Password Function
+
+function factionslogin(event) {
+  event.preventDefault();
+  const correctPassword = "8915534";
+  const input = document.getElementById("factions-password").value;
+
+  if (input === correctPassword) {
+    window.location.href = "8915534-factions.html";
+  } else {
+  }
+}
+
+// Map Password Function
+
+function maplogin(event) {
+  event.preventDefault();
+  const correctPassword = "#";
+  const input = document.getElementById("map-password").value;
+
+  if (input === correctPassword) {
+    window.location.href = "#";
+  } else {
+  }
+}
+
+// Corrupt Password Function
+
+function corruptlogin(event) {
+  event.preventDefault();
+  const correctPassword = "#";
+  const input = document.getElementById("corrupt-password").value;
+
+  if (input === correctPassword) {
+    window.location.href = "#";
+  } else {
+  }
+}
+
+// Template Password Function
+
+function templatelogin(event) {
+  event.preventDefault();
+  const correctPassword = "#";
+  const input = document.getElementById("template-password").value;
+
+  if (input === correctPassword) {
+    window.location.href = "#";
+  } else {
+  }
+}
